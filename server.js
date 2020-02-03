34621854a2018b34e74cd52c847fa9b6c1cdf576
DoMaBoy <45628902+DoMaBOY@users.noreply.github.com>
2020-02-03 20:32:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -136,46 +136,6 @@ client.on("message", async message => {
   }
 });
 
-//----------------------=[ W E A T H E R ]---[ C O M M A N D]=-----------------------//
-
-client.on("message", message => {
-  let msg = message.content.toUpperCase();
-  let sender = message.author;
-  let cont = message.content.slice(prefix.length).split(" ");
-  let args = cont.slice(1);
-
-  if (msg.startsWith(prefix + "WEATHER")) {
-    weather.find({ search: args.join(" "), degreeType: "F" }, function(
-      err,
-      result
-    ) {
-      //
-      if (err) message.channel.send(err);
-
-      if (result.length === 0) {
-        message.channel.send("**Please enter a valid location.**");
-        return;
-      }
-
-      var current = result[0].current;
-      var location = result[0].location;
-
-      const embed = new Discord.RichEmbed()
-        .setDescription(`**${current.skytext}**`)
-        .setAuthor(`Weather for ${current.observationpoint}`)
-        .setThumbnail(current.imageUrl)
-        .setColor(0x00ae86)
-        .addField("Timezone", `UTC${location.timezone}`, true)
-        .addField("Degree Type", location.degreetype, true)
-        .addField("Temperature", `${current.temperature} Degrees`, true)
-        .addField("Feels Like", `${current.feelslike} Degrees`, true)
-        .addField("Winds", current.winddisplay, true)
-        .addField("Humidity", `${current.humidity}%`, true);
-
-      message.channel.send({ embed });
-    });
-  }
-});
 
 client.on("message", message => {
   if (!message.channel.guild) return;
